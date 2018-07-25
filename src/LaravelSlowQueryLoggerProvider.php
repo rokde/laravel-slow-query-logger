c446e6ed4727c7d91518b73d79547283d08b9b5d
rok <rok@ipunkt.biz>
2018-07-25 18:32:29 +0200

    use settings and updated code for laravel 5.6

--- src/LaravelSlowQueryLoggerProvider.php
+++ src/LaravelSlowQueryLoggerProvider.php
@@ -2,73 +2,72 @@
 
 namespace Rokde\LaravelSlowQueryLogger;
 
-use Illuminate\Contracts\Events\Dispatcher;
+use Exception;
 use Illuminate\Database\Events\QueryExecuted;
-use Illuminate\Log\Writer;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Psr\Log\LoggerInterface;
 
 class LaravelSlowQueryLoggerProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     * @param Dispatcher $events
-     * @param Writer $log
-     */
-    public function boot(Dispatcher $events, Writer $log)
-    {
-        $this->publishes([
-            __DIR__ . '/../config/slow-query-logger.php' => config_path('slow-query-logger.php'),
-        ], 'config');
+	/**
+	 * Bootstrap the application services.
+	 * @param LoggerInterface $log
+	 */
+	public function boot(LoggerInterface $log)
+	{
+		if ($this->app->runningInConsole()) {
+			$this->publishes([
+				__DIR__ . '/../config/slow-query-logger.php' => config_path('slow-query-logger.php'),
+			], 'config');
+		}
 
-        $this->setupListener($events, $log);
-    }
+		$this->setupListener($log);
+	}
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/slow-query-logger.php', 'slow-query-logger'
-        );
-    }
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->mergeConfigFrom(
+			__DIR__ . '/../config/slow-query-logger.php', 'slow-query-logger'
+		);
+	}
 
-    /**
-     * setting up listener
-     *
-     * @param Dispatcher $events
-     * @param Writer $log
-     */
-    private function setupListener(Dispatcher $events, Writer $log)
-    {
-        $environments = config('slow-query-logger.environments', []);
+	/**
+	 * setting up listener
+	 *
+	 * @param LoggerInterface $log
+	 */
+	private function setupListener(LoggerInterface $log)
+	{
+		if (!config('slow-query-logger.enabled')) {
+			return;
+		}
 
-        if (!$this->app->environment($environments)) {
-            return;
-        }
+		DB::listen(function (QueryExecuted $queryExecuted) use ($log) {
+			$sql = $queryExecuted->sql;
+			$bindings = $queryExecuted->bindings;
+			$time = $queryExecuted->time;
 
-        $events->listen(QueryExecuted::class, function (QueryExecuted $queryExecuted) use ($log) {
-            $sql = $queryExecuted->sql;
-            $bindings = $queryExecuted->bindings;
-            $time = $queryExecuted->time;
+			$logSqlQueriesSlowerThan = (float) config('slow-query-logger.time-to-log', -1);
+			if ($logSqlQueriesSlowerThan < 0 || $time < $logSqlQueriesSlowerThan) {
+				return;
+			}
 
-            $logSqlQueriesSlowerThan = config('slow-query-logger.time-to-log');
-            if ($logSqlQueriesSlowerThan < 0 || $time < $logSqlQueriesSlowerThan) {
-                return;
-            }
+			$level = config('slow-query-logger.log-level', 'debug');
+			try {
+				foreach ($bindings as $val) {
+					$sql = preg_replace('/\?/', "'{$val}'", $sql, 1);
+				}
 
-            $level = config('slow-query-logger.log-level', 'debug');
-            try {
-                foreach ($bindings as $val) {
-                    $sql = preg_replace('/\?/', "'{$val}'", $sql, 1);
-                }
-
-                $log->log($level, $time . '  ' . $sql);
-            } catch (\Exception $e) {
-                //  be quiet on error
-            }
-        });
-    }
+				$log->log($level, $time . '  ' . $sql);
+			} catch (Exception $e) {
+				//  be quiet on error
+			}
+		});
+	}
 }
