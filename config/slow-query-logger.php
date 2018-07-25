9d0b4941cc512d403540a40c625ed66fff07ee23
rok <rok@ipunkt.biz>
2018-07-25 18:32:07 +0200

    changed settings

--- config/slow-query-logger.php
+++ config/slow-query-logger.php
@@ -1,21 +1,19 @@
 <?php
 
 return [
-    /**
-     * log all sql queries that are slower than X seconds
-     * laravel measures at a precision of 2 digits, so 0.7134 will be logged as 0.71
-     */
-    'time-to-log' => 0.7,
+	/**
+	 * log all sql queries that are slower than X seconds
+	 * laravel measures at a precision of 2 digits, so 0.7134 will be logged as 0.71
+	 */
+	'time-to-log' => env('LARAVEL_SLOW_QUERY_LOGGER_TIME_TO_LOG', 0.7),
 
-    /**
-     * log when you are on these environments
-     */
-    'environments' => [
-        'local',
-    ],
+	/**
+	 * log when you are on these environments
+	 */
+	'enabled' => env('LARAVEL_SLOW_QUERY_LOGGER_ENABLED', false),
 
-    /**
-     * level to log
-     */
-    'log-level' => 'debug',
+	/**
+	 * level to log
+	 */
+	'log-level' => env('LARAVEL_SLOW_QUERY_LOGGER_LOG_LEVEL', 'debug'),
 ];
